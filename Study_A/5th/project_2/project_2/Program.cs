2621f4862a6c331cd77c631e36f8e9a6549dee69
kkisim <163362240+kkisim@users.noreply.github.com>
2025-04-01 16:51:52 +0900

    5주차 강의

---
+++ Study_A/5th/project_2/project_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("나이 입력 : ");
+            //int age = int.Parse(Console.ReadLine());
+
+            //if (age >= 18)
+            //{
+            //    Console.WriteLine("성인입니다.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("미성년자입니다.");
+            //}
+            //Console.WriteLine("숫자입력 : ");
+            //int input = int.Parse(Console.ReadLine());
+            //if(input % 2 == 0)
+            //{
+            //    Console.WriteLine("짝수입니다.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("홀수입니다.");
+            //}
+            String input = Console.ReadLine();
+            int num = int.Parse(input);
+
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number % 2 == 0 ? true : false);
+            Console.WriteLine(number % 2 == 0 ? "짝수입니다." : "홀수입니다.");
+        }
+    }
+}
---
+++ Study_A/5th/sin_5/sin_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sin_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("이름 입력 : ");
+            //String name = Console.ReadLine();
+
+            //Console.WriteLine("나이 입력 : ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine("5년 후의 나이는 "+(age + 5)+"살입니다");
+            //Console.WriteLine("이름은 " + name + "이고 나이는 " + age + "살입니다");
+            //Console.WriteLine("이름은 {0}이고 나이는 {1}살입니다", name, age);
+
+            Console.WriteLine("원의 반지름 입력 : ");
+            double radius = double.Parse(Console.ReadLine());
+            double area = radius * radius * Math.PI;
+            double circumference = 2 * radius * Math.PI;
+            Console.WriteLine("원의 넓이는 " + area + "이고 원의 둘레는 " + circumference + "입니다");
+            Console.WriteLine("원의 둘레 : {0}, 원의 넓이 : {1}", circumference, area);
+            Console.WriteLine("원의 둘레 : "+(2 * radius * Math.PI) + ", 원의 넓이 : " + (radius * radius * Math.PI));
+
+
+        }
+    }
+}
